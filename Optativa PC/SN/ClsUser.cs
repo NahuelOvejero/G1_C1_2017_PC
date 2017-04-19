07b0d2211f559ac7817616fd2362bf9800500f01
peco8015 <claudiolucero_2@hotmail.com>
2017-04-19 01:21:55 -0300

    clase usuario(nombe,pass,puntos)

---
+++ Optativa PC/SN/ClsUser.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SN
+{
+    class ClsUser
+    {
+        string usuario;
+        string pasword;
+        int puntos;
+        public string Usuario
+        {
+            get
+            {
+                return usuario;
+            }
+
+            set
+            {
+                usuario = value;
+            }
+        }
+
+        public string Pasword
+        {
+            get
+            {
+                return pasword;
+            }
+
+            set
+            {
+                pasword = value;
+            }
+        }
+
+        public int Puntos
+        {
+            get
+            {
+                return puntos;
+            }
+
+            set
+            {
+                puntos = value;
+            }
+        }
+    }
+}
