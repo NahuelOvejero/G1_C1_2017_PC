448201708ef9c985b2750e88628e62adc9a5bc75
peco8015 <claudiolucero_2@hotmail.com>
2017-05-02 00:55:05 -0300

    clase.

---
+++ Optativa PC/SN/clsUsuario.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SN
+{
+    public class clsUsuario
+    {
+        string user;
+        int puntos;
+
+        public string User
+        {
+            get
+            {
+                return user;
+            }
+
+            set
+            {
+                user = value;
+            }
+        }
+
+        public int Puntos
+        {
+            get
+            {
+                return puntos;
+            }
+
+            set
+            {
+                puntos = value;
+            }
+        }
+    }
+}
