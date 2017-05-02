1ca3cbc1237fe4e7faf34ea7baf7061e318c3a4a
peco8015 <claudiolucero_2@hotmail.com>
2017-05-02 02:38:09 -0300

    funciones q simulan ser las del servidor

--- Optativa PC/SN/clsComunicacion.cs
+++ Optativa PC/SN/clsComunicacion.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SN
 {
-    class clsComunicacion
+    public class clsComunicacion
     {
+        
+
+        public void enviaEjes( int x1, int y1,frmPrueba formu)
+        {
+            formu.dibujar(x1, y1);
+
+        }
+
+       public void enviaRta(string rta,frmJuego formu)
+        {
+            formu.rtasIncorrectas(rta);
+        }
     }
 }
